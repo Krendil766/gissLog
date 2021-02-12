6985477cd4f843b53999a4771ef0e110c1f3cd9d
Krendil766 <krendilek766@gmail.com>
2021-02-12 16:32:25 +0300

    Finish project

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 let regStr = /\s{4,}/ || /--->/;
-let regName = /\w*\@[0-9A-Za-zА-Яа-я]*-[0-9A-Za-zА-Яа-я]*/ig;
-let regDate = /\d\d:\d\d:\d\d/g;
+let regName = /\w*\@/ig;
+let regDate = /\d\d:\d\d:\d\d/;
 let regIndex = /\d\d\d/;
-
 let logItem = document.querySelector('.log_item');
 let sectionTable = document.querySelector('.section_table')
 let tableItem = document.querySelector('.table_item');
 let table = document.querySelector('.table')
 
-
 function createTextLog(file) {
     let rawFile = new XMLHttpRequest();
     rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function() {
+    rawFile.onreadystatechange = function () {
         if (rawFile.readyState === 4) {
             if (rawFile.status === 200 || rawFile.status == 0) {
                 let allText = rawFile.responseText;
@@ -32,10 +30,9 @@ function createTextLog(file) {
                 }
                 let today = `${day}/${mon}/${year}`;
                 let arrLog = Array.from((allText.split(regStr)));
-                //let indexToday = arrLog.findIndex(item => item.includes(today))
-                //let todayArr = arrLog.slice(indexToday)
-                //logItem.innerHTML = todayArr.map(item => item + '</br>');
-                logItem.innerHTML = arrLog.map(item => item + '</br>');
+                let indexToday = arrLog.findIndex(item => item.includes(today))
+                let todayArr = arrLog.slice(indexToday)
+                logItem.innerHTML = todayArr.map(item => item + '</br>');
             }
         }
     }
@@ -43,69 +40,60 @@ function createTextLog(file) {
 }
 
 function createArrOfLog(param) {
-    let arrDateRegistration;
-    let arrIdUsers;
-    let arrCountRegistrationUsers;
-    let arrUsersRegistration;
     let arrReg = [];
-    let newArr = [];
-    let arrClos = [];
-    let cv;
-
     let arrStr = Array.from(logItem.innerHTML.split('<br>,'));
-
     arrStr.filter((item, index, arr) => {
-            let objReg = {};
-            let arrCount = []
-            if (item.includes('Registration of the user')) {
-                objReg.regIndex = item.match(regIndex).join('');
-                objReg.name = item.match(regName).join('');
-                objReg.dateOn = (item.match(regDate)).join('');
-                objReg.dateOff = (item.match(regDate)).join('');
-                objReg.registr = true;
-                arrCount = arr.filter((item, index, arr) => {
+        let objReg = {};
+        let arrCountConect = [];
+        let arrCountAddedObj = [];
+        let c = 0;
+        if (item.includes('Registration of the user')) {
+            objReg.regIndex = item.match(regIndex).join('');
+            objReg.name = (item.match(regName).join('')).slice(0, (item.match(regName).join('')).length - 1);
+            objReg.dateOn = (item.match(regDate)).join('');
+            objReg.dateOff = (item.match(regDate)).join('');
+            objReg.registr = true;
+            arrCountConect = arr.filter((item, index, arr) => {
+                if (item.includes('Registration of the user')) {
                     if (item.includes(objReg.name)) {
-                        arr.forEach(item => {
-                            if (item.includes('Connection with a server is close')) {
+                        arr.forEach(item2 => {
+                            if (item2.includes('Connection with a server is close')) {
                                 if (item.includes(objReg.regIndex)) {
-                                    objReg.dateOff = (item.match(regDate)).join('')
+                                    objReg.dateFin = (item2.match(regDate)).join('')
                                 }
                             }
                         })
-                        return objReg.dateFin = (item.match(regDate)).join('');
+                        return objReg.dateOff = item.match(regDate).join('');
                     }
+                }
+            })
+            for (let i = 0; i < arr.length; i++) {
+                let count = 0;
+                if (arr[i].includes('Open map. Name =')) {
+                    if (arr[i].includes(objReg.name))
+                        for (let j = i; j < arr.length; j++) {
+                            if (arr[j].includes('Append object successful, object number')) {
+                                count++
+                            } else if (arr[j].includes('Close map, number') || arr[j].includes('Connect timeout (min)')) {
+                                break;
+                            }
+                        }
+                    c = count;
+                    arrCountAddedObj.push(objReg.name, c)
+                }
+                objReg.countAddObj = arrCountAddedObj.filter(item => {
+                    return item > 0 ? item : 0
                 })
-                objReg.countReg = (arrCount.length) / 2;
-                arrReg.push(objReg);
 
 
             }
-
-            /*             newArr = arrReg.filter(item => {
-                            if (item.name == objReg.name) {
-                                newArr.push(item)
-                            }
-                        }) */
-        })
-        /*     let objNew = arrReg.reduce((prev, item) => {
-            return (typeof prev[item.name] !== 'undefined') ? {...prev, [item.name]: prev[item.name] + 1 } : {...prev, [item.name]: 1 }
-        }, {});
-        let arrCount = Object.keys(objNew).map((key) => ({
-            [key]: objNew[key]
-        })) */
-
-    let arr = [];
-    arrReg.forEach((item, i, array) => {
-        if (!arr.length) {
-            arr.push(item)
-        } else {
-            let a = arr.find(tmp => tmp !== item)
-            arr.push(a)
+            objReg.countReg = (arrCountConect.length);
+            arrReg.push(objReg);
         }
     })
-    console.log(arr);
 
-    return arrReg;
+    let unique = arrReg.filter((set => item => !set.has(item.name) && set.add(item.name))(new Set));
+    return unique;
 }
 
 function createTable(good) {
@@ -113,16 +101,15 @@ function createTable(good) {
         table.insertAdjacentHTML('beforeend', `
         <table class="table">
         <tr>
-            <td>${index++}</td>
+            <td>${++index}</td>
             <td>${item.name}</td>
             <td>${item.dateOn}</td>
             <td>${item.dateOff}</td>
             <td>${item.dateFin}</td>
             <td>${item.countReg}</td>
-            <td></td>
+            <td>${item.countAddObj}</td>
         </tr>
     </table>`)
     })
-
 }
-createTable(createArrOfLog(createTextLog("./gisserver.20190901.log")))
+createTable(createArrOfLog(createTextLog("./gisserver.20210201.log")))
